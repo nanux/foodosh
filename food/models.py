7df9f1778dd160a672676444e25585ddcabb5a88
Adam Brokes <abrokes@atlassian.com>
2017-02-23 12:41:26 +1100

    Add the description to the Sides

--- food/models.py
+++ food/models.py
@@ -97,6 +97,7 @@ class Meal(models.Model):
 class Side(models.Model):
     name = models.CharField(max_length=200, unique=True)
     price = models.FloatField()
+    description = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return self.name
--- food/views.py
+++ food/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
 
-from food.models import Meal
+from food.models import Meal, Side
 
 
 def index(request):
     meals = Meal.objects.all()
     template = loader.get_template('food/index.html')
     meals = meals.filter(id__in=[1,2,3,4,5])
+    veggies = Side.objects.get(name='Veggies')
+    late_tea = Side.objects.get(name='Late afternoon tea')
     context = {
         'meals': meals,
+        'veggies': veggies,
+        'late_tea': late_tea
     }
     return HttpResponse(template.render(context, request))
