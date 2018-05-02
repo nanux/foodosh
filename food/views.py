5b8c7470186f11c501eca76eae3af8173ebc9316
Adam Brokes <abrokes@atlassian.com>
2018-05-02 17:30:08 +1000

    update default page

--- food/views.py
+++ food/views.py
@@ -6,7 +6,7 @@ from django.template import loader
 from food.models import Side, Term, MealPlan, IngredientInMeal
 
 
-def index(request, year=2017, term=4, week=1):
+def index(request, year=2018, term=2, week=1):
     template = loader.get_template('food/index.html')
 
     term = Term.objects.get(year=year, term=term)
