a891f94208e0b79f30587c2207156930d54d9fc5
Adam Brokes <adam.brokes@gmail.com>
2019-03-12 22:21:18 +1100

    Update django to 2.1.7 Remove required term definition and automatically get term from date Print weeks automatically Add tests for the get_term_from_date function

--- food/tests.py
+++ food/tests.py
@@ -24,3 +24,33 @@ class MealPlanTestCase(TestCase):
         self.assertEqual(len(list(filter(lambda meal: meal != None, meals))), 1)
         self.assertIn(meal_wed, meals)
         self.assertNotIn(meal_mon, meals)
+
+
+class TermTestCase(TestCase):
+
+    def test_end_date_autofill(self):
+        term1 = Term.objects.create(term=1, year=2019, weeks=1, start=datetime.date(2019, 1, 1))
+        self.assertEqual(term1.end, datetime.date(2019, 1, 8))
+
+    def test_get_term_when_date_in_term(self):
+        first_date = datetime.date(2019, 1, 1)
+        term_expected = Term.objects.create(term=1, year=2019, weeks=1, start=datetime.date(2019, 1, 1))
+        term_actual = Term.get_term_from_date(first_date)
+
+        self.assertEqual(term_expected, term_actual)
+
+    def test_get_term_when_date_between_terms(self):
+        term_first = Term.objects.create(term=1, year=2019, weeks=1, start=datetime.date(2019, 1, 1))
+        term_second = Term.objects.create(term=1, year=2021, weeks=1, start=datetime.date(2021, 1, 1))
+        date_between = datetime.date(2020, 1, 1)
+
+        actual_term = Term.get_term_from_date(date_between)
+        self.assertEqual(actual_term, term_second)
+
+    def test_get_term_when_no_future_or_current_term(self):
+        term_in_past = Term.objects.create(term=1, year=2019, weeks=1, start=datetime.date(2019, 1, 1))
+        term_in_past_past = Term.objects.create(term=1, year=2018, weeks=1, start=datetime.date(2018, 1, 1))
+        date_now = datetime.date(2020, 1, 1)
+
+        actual_term = Term.get_term_from_date(date_now)
+        self.assertEqual(actual_term, term_in_past)
